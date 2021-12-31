b9c35ee9ee51e831f4e7862230117c57b4cd8c32
promehedi <mmhasan563@gmail.com>
2022-01-01 05:30:16 +0600

    Fix Responsive Issue

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -20,8 +20,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const Welcome = () => {
   return (
     <div className='flex w-full justify-center items-center'>
-      <div className='flex mf:flex-row flex-col items-center md:flex-row items-start justify-between md:p-20 py-12 px-4'>
-        <div className='flex flex-1 justify-start items-start flex-col mf:mr-10'>
+      <div className='flex lg:flex-row flex-col items-center justify-between md:p-20 py-12 px-4'>
+        <div className='flex flex-1 justify-start items-start flex-col md:mr-10'>
           <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
             Send Crypto <br /> across the world
           </h1>
@@ -51,8 +51,8 @@ const Welcome = () => {
           </div>
         </div>
 
-        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-          <div className='p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism '>
+        <div className='flex flex-col md:flex-row lg:flex-col flex-1 items-center justify-start w-full lg:mt-0 mt-10'>
+          <div className='p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 md:mr-4 lg:mr-0 eth-card .white-glassmorphism '>
             <div className='flex justify-between flex-col w-full h-full'>
               <div className='flex justify-between items-start'>
                 <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
