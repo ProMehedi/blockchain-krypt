fefd3bf61d82a9f87fd56e4e6247c9d991ee2bc6
promehedi <mmhasan563@gmail.com>
2022-01-09 03:23:44 +0600

    Fix useFetch hook

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 
-const API_KEY = process.env.REACT_APP_GIPHY_API
+const APIKEY = process.env.REACT_APP_GIPHY_API
 
 const useFetch = ({ keyword }) => {
-  const [url, setUrl] = React.useState('')
+  const [gifUrl, setGifUrl] = React.useState('')
+
+  const fetchGifs = async () => {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword
+          .split(' ')
+          .join('')}&limit=1`
+      )
+      const { data } = await response.json()
+
+      setGifUrl(data[0]?.images?.downsized_medium.url)
+    } catch (error) {
+      setGifUrl(
+        'https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284'
+      )
+    }
+  }
 
   React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&q=${keyword}&limit=1`
-        )
-        const { data } = await response.json()
-        setUrl(data[0]?.images?.downsized_medium?.url)
-      } catch (error) {
-        console.log(error)
-        setUrl('https://acegif.com/wp-content/uploads/gif-shaking-head-38.gif')
-      }
-    }
+    if (keyword) fetchGifs()
 
-    keyword && fetchData()
+    // eslint-disable-next-line
   }, [keyword])
 
-  return url
+  return gifUrl
 }
 
 export default useFetch
