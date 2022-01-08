636d6592504312c84543ae348c368c551ff8753e
promehedi <mmhasan563@gmail.com>
2022-01-09 00:33:57 +0600

    Create Transactions Section

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,5 +1,21 @@
+import React from 'react'
+import { TransactionContext } from '../context/TransactionContext'
+import dummyData from '../utils/dummyData'
+
 const Transactions = () => {
-  return <div>Transaction</div>
+  const { transactions, currentAcc } = React.useContext(TransactionContext)
+
+  return (
+    <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
+      <div className='flex flex-col md:p-12 py-12 px-4'>
+        <h3 className='text-white text-3xl text-center my-2'>
+          {currentAcc
+            ? 'Latest Transactions'
+            : 'Connect your account to see the latest transactions'}
+        </h3>
+      </div>
+    </div>
+  )
 }
 
 export default Transactions
