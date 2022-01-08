42070d1380adb5f9d3d5bd402f0b32af93b0430e
promehedi <mmhasan563@gmail.com>
2022-01-09 03:54:14 +0600

    Redepoly Fresh Contract

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json'
 
 export const contractABI = abi.abi
-export const contractAddress = '0x0a5F941eb4cFEf83d4823FFc0399aAcfA4c23a3b'
+export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS
